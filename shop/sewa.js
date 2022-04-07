437eb697bcef223308bd8b1b8047abf6afb81805
rexxinbot <102780669+rexxinbot@users.noreply.github.com>
2022-04-08 05:01:04 +0700

    Add files via upload

---
+++ shop/sewa.js
@@ -0,0 +1,23 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 1.000
+⬡ 2 MINGGU = Rp 3.000
+⬡ 3 MINGGU = Rp 5.000
+⬡ PREMIUM 2.000
+⬡ PERMANEN + PREM = Rp 10.000
+
+⬡ PULSA PERMANEN + PREM = 15.000
+
+
+⬣ MINAT? PM
+⬡ wa.me/6281317549796?text=Min+Sewa+Bot
+
+
+NOTE : 
+Bot On 24 Jam Jika Tidak Ada Kesalahan Pada Jaringan `
+	}
+
+exports.pc_sewa = pc_sewa
